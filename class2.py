5849280f6714c6335fdac10721f324829551648e
Terminator19220 <sinaijudah@gmail.com>
2025-05-21 12:44:32 -0400

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,25 @@
+"""Activity 1
+Welcome to the world of coding
+Outline:
+Write a program for print command."""
+
+print("Welcome to the world of programming.\n I am learning python")
+
+"""Activity 2 Understanding the variables
+Outline:
+Write a program to create different variables to store various values."""
+name=("Sinai")
+print(name)
+age=(13)
+print(age)
+
+"""Activity 3
+Keywords
+Outline:
+Write a program to print all the keywords"""
+
+import keyword
+print(keyword.kwlist)
+a=input("what is your name? ")
+print(a)
+
